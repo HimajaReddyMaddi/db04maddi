d9ac486fc5abdff4bc715b3999f635dfc6f5be1a
HimajaReddyMaddi <S545156@nwmissouri.edu>
2021-11-03 19:54:38 -0500

    added mat.pug and js file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var matRouter = require('./routes/mat');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mat', matRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
