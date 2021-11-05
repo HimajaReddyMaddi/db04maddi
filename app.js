49f56e8726e930d61975ee181e0fa667aee9fc78
HimajaReddyMaddi <S545156@nwmissouri.edu>
2021-11-05 17:35:34 -0500

    modified app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var matRouter = require('./routes/Mat');
+var matRouter = require('./routes/mat');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 
