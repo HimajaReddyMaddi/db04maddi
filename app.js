eecc7b722c89fa4026eeea12a5881ad42aefebb5
HimajaReddyMaddi <S545156@nwmissouri.edu>
2021-11-05 17:38:22 -0500

    modified app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var matRouter = require('./routes/mat');
+var matRouter = require('./routes/Mat');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mat', matRouter);
+app.use('/Mat', matRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 
