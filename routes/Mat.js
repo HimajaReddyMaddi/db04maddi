837a282780f27d677c2e73a7dbabe9b66905d816
HimajaReddyMaddi <S545156@nwmissouri.edu>
2021-11-17 18:33:08 -0600

    added create page

--- routes/Mat.js
+++ routes/Mat.js
@@ -6,4 +6,8 @@ router.get('/', mat_controllers.Mat_view_all_Page );
 router.get('/Mat/:id', mat_controllers.mat_detail);
 /* GET detail costume page */
 router.get('/detail', mat_controllers.mat_view_one_Page);
+/* GET create costume page */
+router.get('/create', mat_controllers.mat_create_Page);
+/* GET create update page */
+router.get('/update', mat_controllers.mat_update_Page);
 module.exports = router;
