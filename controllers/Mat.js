90df747ecb1d05acc2ef7cebcc1959ece8a66457
HimajaReddyMaddi <S545156@nwmissouri.edu>
2021-11-17 18:59:22 -0600

    added update page

--- controllers/Mat.js
+++ controllers/Mat.js
@@ -133,3 +133,17 @@ exports.mat_create_Page = function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
     };
+
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.mat_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Mat.findById(req.query.id)
+    res.render('Matupdate', { title: 'Mat Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
